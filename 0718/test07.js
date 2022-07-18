407de30b555536ff3e0b83d4e1307d4ccc13262f
DESKTOP-P2E4GPS\505 <hexun_@naver.com>
2022-07-18 13:47:59 +0900

    2022_07_18 js

--- 0718/test07.js
+++ 0718/test07.js
@@ -1,2 +1,6 @@
 // 배열의 특정 요소 위치 확인
 // 배열.indexOf(검색할 값, 시작 인덱스)
+const arr = ['spring','summer','fall','winter','is','down']
+
+console.log(`"winter" is in this index ${arr.indexOf('winter')}`);
+console.log(`"winter" is not in this value ${arr.indexOf('winter',4)}`);
---
+++ 0718/test08.js
@@ -0,0 +1,11 @@
+// 
+const arr = [
+    {id : 0, name : '말자', age : 6},
+    {id : 1, name : '여자', age : 3},
+    {id : 2, name : '공자', age : 8},
+    {id : 3, name : '순자', age : 5}
+];
+
+arr.forEach((el) => {
+    console.log(el.name);
+});
